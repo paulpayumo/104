e97b1ea683efab66554301084e885ca1f2714b38
paulpayumo <60272942+paulpayumo@users.noreply.github.com>
2020-10-21 20:01:52 -0700

    Update Project 2.py

--- Project 2.py
+++ Project 2.py
@@ -8,31 +8,34 @@ import pandas as pd
 # DATA SET UP
 data = pd.read_csv('OWCoronaCasesTest.csv' , sep = ',')
 data = data[['ID','World']]
-print(data.head())
+data = data[60:150]
+print(data)
 
 x = np.array(data['ID']).reshape(-1,1)
 y = np.array(data['World']).reshape(-1,1)
-plt.plot(y,'-b')
+plt.plot(y,'-b', label = "Actual World Cases")
 
-poly = PolynomialFeatures(degree=3)
+poly = PolynomialFeatures(degree=2)
 x = poly.fit_transform(x)
-#print(x)
 
 #TRAINING DATA
 model = linear_model.LinearRegression()
 model.fit(x,y)
 accuracy = model.score(x,y)
 print('Accuracy:',round(accuracy*100,3),'%')
-
+"""
 f = model.predict(x)
 plt.plot(f, '--r')
-
+"""
 #PREDICTION
-days = 2
-print('Predition of Cases after',{days},'days:')
-print((round(int(model.predict(poly.fit_transform([[294+days]]))))/1000000,2),'Million')
+print('Predition of Cases at day 240:')
+print((round(int(model.predict(poly.fit_transform([[240]]))))/1000000),'Million')
 
-x1 = np.array(list(range(1,234+days))).reshape(-1,1)
+x1 = np.array(list(range(151,241))).reshape(-1,1)
 y1 = model.predict(poly.fit_transform(x1))
-plt.plot(y1, '--g')
+y= np.concatenate((y, y1), axis = 0)
+plt.plot(y, '--g', label = "Prediction")
+plt.legend()
+plt.ylabel('World Cases x(1e7)')
+plt.xlabel('Day of the Year(0 = 60 days)')
 plt.show()
